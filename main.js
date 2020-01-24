3cdd23c88a63d982719f1be3d9616df6127e9282
romangibb20 <60204005+romangibb20@users.noreply.github.com>
2020-01-24 18:13:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,327 @@
+require([
+        "dojo/on",
+        "esri/Map",
+        "esri/views/SceneView",
+        "esri/widgets/Expand",
+        "esri/widgets/Search",
+        "esri/tasks/Locator",
+        "esri/Camera",
+        "esri/geometry/geometryEngine",
+        "esri/Graphic",
+        "esri/symbols/SimpleFillSymbol",
+        "esri/symbols/SimpleLineSymbol",
+        "esri/widgets/Sketch/SketchViewModel",
+        "esri/layers/GraphicsLayer",
+      ], function(on, Map, SceneView, Expand, Search, Locator, Camera, geometryEngine, Graphic, SimpleFillSymbol, SimpleLineSymbol, SketchViewModel, GraphicsLayer) {
+  
+        var searchExpand, solarExpand, sketchViewModel, panelGraphic;
+        var drawButton = document.getElementById("drawButton");
+        var calcButton = document.getElementById("calcButton");
+  
+        <!-- ***** SETUP WIDGETS AND DISPLAY ***** !-->
+      
+        // Use an ESRI basemap
+        var map = new Map({
+          basemap: "hybrid",
+          ground: "world-elevation"
+        });
+
+        // Create a 3D view
+        var view = new SceneView({
+          container: "viewDiv",
+          map: map,
+          camera: new Camera({
+            position: [-100.4593, 16.9014, 5500000],
+            heading: 10.53,
+            tilt: 20
+          })
+        });
+  
+        // Use a graphics layer for drawing polygons/panels
+        var graphicsLayer = new GraphicsLayer({
+          id: "sp",
+          title: "Solar Panels",
+          visible: true
+        });
+        map.add(graphicsLayer);
+        
+        // Use ESRI's world geocoder to locate addresses
+        // but limit results to US addresses only as per spec
+        var locatorUrl = "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/";
+  
+        var usLocator = new Locator({
+              url: locatorUrl
+        });
+   
+        // Use the search widget with source set to locator
+        var searchWidget = new Search({
+          view: view,
+          popupEnabled: false,
+          sources: [{
+            locator: usLocator,
+            countryCode: "US",
+            placeholder: "Enter a US address"
+          }],
+          includeDefaultSources: false 
+        });
+   
+        // Add search and calculator functionality 
+        // to Expand widget to reduce clutter
+  
+        searchExpand = new Expand({
+          expandIconClass: "esri-icon-search",
+          expandTooltip: "Expand Search",
+          expanded: false,
+          view: view,
+          group: "top-right",
+          content: searchWidget
+        });
+  
+        solarExpand = new Expand({
+          expandIconClass: "esri-icon-polygon",
+          expandTooltip: "Expand Solar Calculator",
+          expanded: false,
+          view: view,
+          group: "top-right",
+          content: document.getElementById("npArea"),
+        });
+
+        // Add the expand widget to the top right corner of the view
+        view.ui.add([searchExpand, solarExpand], "top-right"); 
+  
+        <!-- ***** SETUP LISTENERS AND HANDLERS ***** !-->
+  
+        view.when(function() { //callback when the view renders
+
+          // Listeners 
+          drawButton.addEventListener("click", drawPanels);
+          calcButton.addEventListener("click", calcNominalPower);
+          
+          // Set up sketchviewmodel for drawing panel polygons
+          sketchViewModel = new SketchViewModel({
+            view: view,
+            layer: graphicsLayer,
+            updateOnGraphicClick: true,
+            defaultUpdateOptions: {
+              toggleToolOnClick: false
+            }
+          });
+          
+          // when the user double clicks to complet the
+          // polygon, head to createPanel
+          sketchViewModel.on("create", createPanel);
+              
+          // 'delete' is fired as part of an update event
+          sketchViewModel.on("update", deletePanel);
+                
+        });
+  
+        <!-- ***** FUNCTIONS ***** !-->
+                
+        // the Draw Panel button was clicked
+        function drawPanels(event) {
+          
+          // Remove any previous panels
+          sketchViewModel.cancel();
+          graphicsLayer.removeAll();
+          panelGraphic = undefined;
+          
+          // Hide the previous results
+          document.getElementById("resultArea").style.display = "none";
+          document.getElementById("npArea").style.height = "270px";
+          
+          // Let the widget handle drawing
+          sketchViewModel.create("polygon"); 
+        }
+
+        // The drawing operation ended with a 
+        // double click
+        function createPanel(event) {     
+          
+          if (event.state === "complete") {
+            
+            // Setup the 3D symbol. Get the height from the input
+            var symbol = {
+              type: "polygon-3d",  
+              symbolLayers: [{
+                type: "extrude",  
+                size: document.getElementById("heightInput").value,  // height
+                material: { color: "blue" }
+              }]
+            };
+            
+            // Graphic objects contain geometry and symbols
+            // create and add to the graphics layer
+            panelGraphic = new Graphic({
+              geometry: event.graphic.geometry,
+              symbol: symbol
+            });       
+            graphicsLayer.graphics.add(panelGraphic);
+
+            // Calculate nominal power. panelGraphic is global so 
+            // no parameters here
+            calcNominalPower();
+            
+          } 
+        }
+  
+        // User selected a polygon and clicked
+        // on delete key
+        function deletePanel(event) {
+
+          if (event.state === "cancel") {
+                 
+            //Hide the previous results
+            document.getElementById("resultArea").style.display = "none";
+            document.getElementById("npArea").style.height = "270px";
+            
+            //Remove the sketch graphic
+            sketchViewModel.delete();
+          }
+        }
+  
+        // Function to calculate Nominal Power
+        // E = A x r x H x PR
+        // E: Energy (kWh)
+        // A: total Area of the panel (m²)
+        // r is solar panel yield (%)
+        // H is annual average solar radiation on tilted panels
+        // PR = Performance ratio
+        // Assumptions/Sources for nominal power calculation:
+        // 1) The annual average (H) is 10857.75 MJ/m2 according to 
+        //    http://www.opensolardb.org/db/extractcopypaste. This 
+        //    equates to 3016kWh. Assumptions are optimal tilt
+        //    and constant radiation density across US territories.
+        // 2) Solar panel yeild (r) is assumed to be 15%. Standard test 
+        //    conditions (STC) are assumed: radiation=1000 W/m2, 
+        //    cell temperature=25C, Wind speed=1 m/s, Air Mass=1.5
+        // 3) Performance ratio (PR) equates to .75 and was calculated
+        //    as the product of 1 - the loss details assumed below:
+        //    Inverter losses (6% to 15 %) = 8%
+        //    Temperature losses (5% to 15%) = 8%
+        //    DC cables losses (1 to 3 %) = 2%
+        //    AC cables losses (1 to 3 %) = 2%
+        //    Shadings  0 % to 40% (depends of site) = 3%
+        //    Losses weak irradiation 3% to 7% = 3%
+        //    Losses due to dust, snow... (2%) = 2%
+        //    Other Losses = 0%
+        //    source: https://photovoltaic-software.com/principle-ressources/how-calculate-solar-energy-power-pv-systems
+        function calcNominalPower (){
+          
+          if (panelGraphic == undefined){ //when a new graphic is being drawn or when the application loads
+            alert("Please draw a panel first");
+            return;
+          }
+             
+          // Planar area
+          var pArea = geometryEngine.planarArea(geometryEngine.simplify(panelGraphic.geometry), "square-meters"); 
+                   
+          if (pArea>100000){ //messes with the display, not intended use. 
+            alert("Please zoom to rooftop levels");
+            return;
+          }
+                        
+          // surface area as per tilt (orientation has no impact)     
+          var tilt = document.getElementById("tiltInput").value;
+          
+          if (tilt>89){ 
+            alert("Please enter a tile less than 90 degrees");
+            return;
+          }
+          
+          // Leaving this here since spec requested information be 
+          // collected. not used to calculate surface area using 
+          // Area/Cos(angle) formula
+          
+          /*var azimuth;       
+          switch(document.getElementById("orientationInput").value) {
+            case "North":
+              azimuth = 0;
+              break;
+            case "East":
+              azimuth = 90;
+              break;
+            case "South":
+              azimuth = 180;
+              break;
+            case "West":
+              azimuth = 270;
+              break;
+          }*/
+          
+          // Calculate the surface area under the planar polygon
+          var sArea = calcSurfaceArea(pArea, tilt)
+          
+          // Use the surface area in nominal power calculation
+          var nPower = sArea * .15 * 3016 * .75;
+          
+          // Format things for display
+          var unit 
+          if (nPower > 1000000){
+            unit = "GWh/yr";
+            nPower = nPower / 1000000;
+          } else if (nPower > 1000) {
+            unit = "MWh/yr";
+            nPower = nPower / 1000;
+          } else
+            {
+              unit = "kWh/yr";
+            }
+          var nPowerFmt = nPower.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+          var paFmt = pArea.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+          var saFmt = sArea.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+            
+          // Show the results
+          document.getElementById("npResult").innerHTML = nPowerFmt + " " + unit;
+          document.getElementById("paResult").innerHTML = paFmt + " sqm";
+          document.getElementById("saResult").innerHTML = saFmt + " sqm";
+          
+          document.getElementById("resultArea").style.display = "block";
+          document.getElementById("npArea").style.height = "385px";
+             
+          // Redraw the polygon in case the height was changed
+          // This could be better organized or done only if it was changed
+          refreshSymbol();
+          
+        }
+ 
+        function calcSurfaceArea(pArea, tilt) {
+          
+          //convert deg to rad as cos function expects rad
+          var pi = Math.PI;
+          var tiltRad = tilt * (pi/180);
+         
+          // Formula below
+          // http://www.innovativegis.com/basis/Supplements/BM_Dec_02/Surface_Area3.htm
+          
+          var sArea = pArea / Math.cos(tiltRad);
+          return sArea;
+          
+        }
+  
+        // Redraw the panel when recalculating
+        function refreshSymbol(){
+        
+          var symbol = {
+            type: "polygon-3d",  
+            symbolLayers: [{
+              type: "extrude",  
+              size: document.getElementById("heightInput").value,  // height
+              material: { color: "blue" }
+            }]
+          };
+
+          // Get the previous geometry before recreating
+          var geometry = panelGraphic.geometry
+
+          panelGraphic = new Graphic({
+            geometry: geometry,
+            symbol: symbol
+          });
+
+          graphicsLayer.removeAll();
+          graphicsLayer.graphics.add(panelGraphic);
+        
+        }
+  
+ });
---
+++ readme.txt
@@ -0,0 +1,63 @@
+Name: Roman Gibbons
+Demo Project: A web based solar calculator application
+
+Installation
+
+To install the application, copy image directory and files (index.html, main.css and main.js) to any directory location on your local machine. Launch the index.html file using a modern browser. 
+
+Test Suite
+
+-> Standard operation
+
+1.	Launch index.html in Chrome browser
+2.	Click the magnifier icon on the expand widget
+3.	Enter “1334th Ave, New Auburn, WI, 54757, USA”
+4.	Expand the solar calculator by clicking on the polygon icon on the expand widget
+5.	Leave default values for orientation, tilt and height
+6.	Click on the Draw Solar Panel button
+7.	Draw a polygon of 4 vertices over the brightest area of the structure shown closest to the center of the map
+8.	Double click to end drawing
+9.	Results should be approximately:
+
+Planimetric Area:
+328.82 sqm
+Panel Surface Area:
+328.82 sqm
+Nominal Power:
+111.57 MWh/yr
+
+10.	Change the tilt value to 15 degrees
+11.	Results should be approximately:
+
+Planimetric Area:
+328.82 sqm
+Panel Surface Area:
+340.42 sqm
+Nominal Power:
+115.50 MWh/yr
+
+12.	Change the height value to 10m
+13.	Click the Calculate button
+14.	Use the 3D control to change the tilt of the map
+15.	Results should how 3D polygon
+16.	Click on the polygon
+17.	Press the delete key on the keyboard
+
+-> Error catching operation
+
+1.	Launch index.html in Chrome browser
+2.	Expand the solar calculator by clicking on the polygon icon on the expand widget
+3.	Click on the Draw Solar Panel button
+4.	Draw a polygon anywhere on the map
+5.	Double click to end drawing
+6.	An alert should pop up “Please zoom to rooftop levels”
+7.	Click the magnifier icon on the expand widget
+8.	Enter “1334th Ave, New Auburn, WI, 54757, USA”
+9.	Expand the solar calculator by clicking on the polygon icon on the expand widget
+10.	Click on the Draw Solar Panel button
+11.	Draw a polygon of 4 vertices over the brightest area of the structure shown closest to the center of the map
+12.	Double click to end drawing
+13.	Results should be approximately equal to values in 9 above
+14.	Change the tilt value to 95 degrees
+15.	Click on the Calculate button
+16.	An alert should pop up “Please enter a tilt less than 90 degrees”
